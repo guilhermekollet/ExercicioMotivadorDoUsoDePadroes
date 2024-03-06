874c711731a7a23ccbdeaedf3d86e734df06f52b
Guilherme Serafim Kollet <40221574+guilhermekollet@users.noreply.github.com>
2024-03-06 23:06:23 +0000

    fix flex name car to suv

--- src/App.java
+++ src/App.java
@@ -4,13 +4,13 @@ public class App {
         Carro basico = new Carro("Basico", TipoCombustivel.GASOLINA, 10, 55);
         Carro esportivo = new Carro("Esportivo", TipoCombustivel.GASOLINA, 6, 45);
         Carro utilitario = new Carro("Utilitario", TipoCombustivel.DIESEL, 15, 60);
-        Carro flex = new Carro("Flex", TipoCombustivel.FLEX, 8, 55);
+        Carro suv = new Carro("SUV", TipoCombustivel.FLEX, 8, 55);
 
         System.out.println("Tipos de veiculos:");
         System.out.println(basico);
         System.out.println(esportivo);
         System.out.println(utilitario);
-        System.out.println(flex);
+        System.out.println(suv);
 
         System.out.println("\n\n----------------");
         System.out.println("\nAbastencendo carro basico com gasolina");
@@ -22,9 +22,13 @@ public class App {
         System.out.println("\nAbastencendo carro utilitario com diesel");
         utilitario.abastece(TipoCombustivel.DIESEL, 60);
 
+        System.out.println("\nAbastencendo carro flex com alcool");
+        suv.abastece(TipoCombustivel.ALCOOL, 55);
+
         System.out.println(basico);
         System.out.println(esportivo);
         System.out.println(utilitario);
+        System.out.println(suv);
 
         System.out.println("\nViajando com o carro basico");
         basico.viaja(250);
@@ -40,5 +44,10 @@ public class App {
         utilitario.viaja(250);
         utilitario.viaja(150);
         System.out.println(utilitario);
+
+        System.out.println("\nViajando com o carro flex");
+        suv.viaja(250);
+        suv.viaja(150);
+        System.out.println(suv);
     }
 }
