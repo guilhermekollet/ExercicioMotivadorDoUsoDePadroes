af0b4c8b1c11175f6d91001409f0d2b35d035ca2
Guilherme Serafim Kollet <40221574+guilhermekollet@users.noreply.github.com>
2024-03-06 23:00:40 +0000

    add utilitary car

--- src/App.java
+++ src/App.java
@@ -3,10 +3,12 @@ public class App {
 
         Carro basico = new Carro("Basico", TipoCombustivel.GASOLINA, 10, 55);
         Carro esportivo = new Carro("Esportivo", TipoCombustivel.GASOLINA, 6, 45);
+        Carro utilitario = new Carro("Utilitario", TipoCombustivel.DIESEL, 15, 60);
         
         System.out.println("Tipos de veiculos:");
         System.out.println(basico);
         System.out.println(esportivo);
+        System.out.println(utilitario);
 
         System.out.println("\n\n----------------");
         System.out.println("\nAbastencendo carro basico com gasolina");
